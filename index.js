be2236f6d6552875a68c0146808514d56d1984f7
Airton <airzan7@gmail.com>
2019-08-02 15:14:45 -0300

    basicrequirements

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 $(document).ready(function(){
   var currentQuestion;
+  var interval;
   var timeLeft = 10;
+  var score = 0;
+
+  var updateTimeLeft = function (amount) {
+    timeLeft += amount;
+    $('#time-left').text(timeLeft);
+  };
+
+  var updateScore = function (amount) {
+    score += amount;
+    $('#score').text(score);
+  };
+
+  var startGame = function () {
+    if (!interval) {
+      if (timeLeft === 0) {
+        updateTimeLeft(10);
+        updateScore(-score);
+      }
+      interval = setInterval(function () {
+        updateTimeLeft(-1);
+        if (timeLeft === 0) {
+          clearInterval(interval);
+          interval = undefined;
+        }
+      }, 1000);
+    }
+  };
+
   var randomNumberGenerator = function (size) {
     return Math.ceil(Math.random() * size);
-  }
+  };
 
   var questionGenerator = function () {
     var question = {};
@@ -14,36 +43,26 @@ $(document).ready(function(){
     question.equation = String(num1) + " + " + String(num2);
 
     return question;
-  }
-
-  var updateTimeLeft = function (amount) {
-    timeLeft += amount;
-    $('#time-left').text(timeLeft);
-  }
+  };
 
   var renderNewQuestion = function () {
     currentQuestion = questionGenerator();
     $('#equation').text(currentQuestion.equation);
-  }
+  };
 
   var checkAnswer = function (userInput, answer) {
     if (userInput === answer) {
       renderNewQuestion();
       $('#user-input').val('');
       updateTimeLeft(+1);
+      updateScore(+1);
     }
-  }
+  };
 
   $('#user-input').on('keyup', function () {
+    startGame();
     checkAnswer(Number($(this).val()), currentQuestion.answer);
   });
-
-  var interval = setInterval(function () {
-    updateTimeLeft(-1);
-    if (timeLeft === 0) {
-      clearInterval(interval);
-    }
-  }, 1000);
 
   renderNewQuestion();
 });
