755b5a717d9c43edae1ad333283389f2a63e8d02
Airton <airzan7@gmail.com>
2019-08-02 14:48:30 -0300

    time left p3

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   var currentQuestion;
+  var timeLeft = 10;
   var randomNumberGenerator = function (size) {
     return Math.ceil(Math.random() * size);
   }
@@ -15,21 +16,34 @@ $(document).ready(function(){
     return question;
   }
 
+  var updateTimeLeft = function (amount) {
+    timeLeft += amount;
+    $('#time-left').text(timeLeft);
+  }
+
   var renderNewQuestion = function () {
     currentQuestion = questionGenerator();
     $('#equation').text(currentQuestion.equation);
   }
 
   var checkAnswer = function (userInput, answer) {
-    if(userInput === answer) {
+    if (userInput === answer) {
       renderNewQuestion();
       $('#user-input').val('');
+      updateTimeLeft(+1);
     }
   }
 
   $('#user-input').on('keyup', function () {
     checkAnswer(Number($(this).val()), currentQuestion.answer);
   });
+
+  var interval = setInterval(function () {
+    updateTimeLeft(-1);
+    if (timeLeft === 0) {
+      clearInterval(interval);
+    }
+  }, 1000);
 
   renderNewQuestion();
 });
